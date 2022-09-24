dec4f0d8d401610663d6cabe187216f3c437b0a0
greedy0110 <greedy0110@gmail.com>
2022-09-24 16:23:58 +0900

    add max sort shift

--- src/maxsort.rs
+++ src/maxsort.rs
@@ -1,7 +1,13 @@
 use crate::arraryutility::*;
 
 fn max_sort_shift(v: &mut Vec<i32>) {
-    
+    let end = v.len();
+    for left in 0..end {
+        let i_max = findmaxpos(&v, left, end);
+        let max = v[i_max];
+        shift_right(v, left, i_max + 1);
+        v[left] = max;
+    }
 }
 
 #[cfg(test)]
@@ -9,9 +15,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_max_sort() {
+    fn test_max_sort_shift() {
         let mut v = vec![1, 2, 3, 4, 5];
         max_sort_shift(&mut v);
+        assert_eq!(v, vec![5, 4, 3, 2, 1]);
+    }
+
+    #[test]
+    fn test_max_sort_swap() {
+        let mut v = vec![1, 2, 3, 4, 5];
+        max_sort_swap(&mut v);
         assert_eq!(v, vec![5, 4, 3, 2, 1]);
     }
 }
